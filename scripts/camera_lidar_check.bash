030d09ae2a6d459c6b1f636e73bec81cafdf26c6
Tomoya Muraki <tomoya.muraki@tier4.jp>
2026-01-22 17:11:34 +0900

    fix: pass camera name as arg

--- scripts/camera_lidar_check.bash
+++ scripts/camera_lidar_check.bash
@@ -1,72 +1,70 @@
 #!/bin/bash
 
-PS3='確認するカメラの左の数字(1~8)を入力してください: '
-options=("camera0" "camera1" "camera2" "camera3" "camera4" "camera5" "camera6" "camera7" "camera8" "camera9" "camera10")
-select cameraName in "${options[@]}"
-do
-    case $cameraName in
-        "camera0")
-            camera_name=$cameraName
-            image_frame="top_rear_center"
-            lidar_name="rear_upper"
-            break;;
-        "camera1")
-            camera_name=$cameraName
-            image_frame="left_center"
-            lidar_name="left_lower"
-            break;;
-        "camera2")
-            camera_name=$cameraName
-            image_frame="right_center"
-            lidar_name="right_lower"
-            break;;
-        "camera3")
-            camera_name=$cameraName
-            image_frame="top_front_right"
-            lidar_name="front_upper"
-            break;;
-        "camera4")
-            camera_name=$cameraName
-            image_frame="top_front_center_right"
-            lidar_name="left_upper"
-            break;;
-        "camera5")
-            camera_name=$cameraName
-            image_frame="top_front_left"
-            lidar_name="left_upper"
-            break;;
-        "camera6")
-            camera_name=$cameraName
-            image_frame="left_front"
-            lidar_name="left_upper"
-            break;;
-        "camera7")
-            camera_name=$cameraName
-            image_frame="right_front"
-            lidar_name="right_upper"
-            break;;
-        "camera8")
-            camera_name=$cameraName
-            image_frame="top_front_center_left"
-            lidar_name="left_upper"
-            break;;
-        "camera9")
-            camera_name=$cameraName
-            image_frame="right_rear"
-            lidar_name="right_upper"
-            break;;
-        "camera10")
-            camera_name=$cameraName
-            image_frame="left_rear"
-            lidar_name="left_upper"
-            break;;
-        *)
-            echo "指定されたカメラは存在しません"
-            ;;
-    esac
-done
+cameraName=${1:-"camera0"}
 
-echo "カメラ: $camera_name"
+case $cameraName in
+    camera0)
+        camera_name=$cameraName
+        image_frame="top_rear_center"
+        lidar_name="rear_upper"
+        ;;
+    camera1)
+        camera_name=$cameraName
+        image_frame="left_center"
+        lidar_name="left_lower"
+        ;;
+    camera2)
+        camera_name=$cameraName
+        image_frame="right_center"
+        lidar_name="right_lower"
+        ;;
+    camera3)
+        camera_name=$cameraName
+        image_frame="top_front_right"
+        lidar_name="front_upper"
+        ;;
+    camera4)
+        camera_name=$cameraName
+        image_frame="top_front_center_right"
+        lidar_name="left_upper"
+        ;;
+    camera5)
+        camera_name=$cameraName
+        image_frame="top_front_left"
+        lidar_name="left_upper"
+        ;;
+    camera6)
+        camera_name=$cameraName
+        image_frame="left_front"
+        lidar_name="left_upper"
+        ;;
+    camera7)
+        camera_name=$cameraName
+        image_frame="right_front"
+        lidar_name="right_upper"
+        ;;
+    camera8)
+        camera_name=$cameraName
+        image_frame="top_front_center_left"
+        lidar_name="left_upper"
+        ;;
+    camera9)
+        camera_name=$cameraName
+        image_frame="right_rear"
+        lidar_name="right_upper"
+        ;;
+    camera10)
+        camera_name=$cameraName
+        image_frame="left_rear"
+        lidar_name="left_upper"
+        ;;
+    *)
+        echo "指定されたカメラは存在しません: $cameraName" >&2
+        exit 1
+        ;;
+esac
+
+echo "Camera: $camera_name, Lidar: $lidar_name"
 
 ros2 run tier4_calibration_views image_view_node.py \
     --ros-args \
